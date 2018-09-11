8b2fcfd192c62663584ed972eb76da08d1904ed6
Хахель Владислав <vlad_hahel@mail.ru>
2018-09-11 20:13:27 +0300

    Added a new student

--- git/AvgMarks/Program.cs
+++ git/AvgMarks/Program.cs
@@ -15,20 +15,24 @@ namespace AvgMarks
             Student student3 = new Student(3, "Zhenya", "2-1");
 			Student student4 = new Student(4, "Sonya" , "2-1");
 			Student student5 = new Student(5, "Vlad" , "2-2");
+			Student student6 = new Student(6, "Vova" , "2-1");
 
             student1.Score = new int[] {9,10,10,10,10};
             student2.Score = new int[] {4,5,6,4,4,4,5,6,8,5,6};
             student3.Score = new int[] { 2,10,5};
 			student4.Score = new int[] {10,10,10,10,10,10,10,10,10,10,10,10,10,10,1};
 			student3.Score = new int[] { 2,3,5};
+			student3.Score = new int[] { 4,6,8,10};
             double []mas = new double[] { student1.GetAvgScore(student1.Score),
 			student2.GetAvgScore(student2.Score), student3.GetAvgScore(student3.Score),
-			student4.GetAvgScore(student4.Score),student5.GetAvgScore(student5.Score)};
+			student4.GetAvgScore(student4.Score),student5.GetAvgScore(student5.Score),
+			student6.GetAvgScore(student6.Score)};
             student1.GetInfo();
             student2.GetInfo();
             student3.GetInfo();
 			student4.GetInfo();
 			student5.GetInfo();
+			student6.GetInfo();
             Console.WriteLine("Students Count: {1},Group AvgScore: {0}",Student.GetGroupAvgScore(mas), Student.studentsCount);
 
             
