9dbf75a85290612540adc1370e06e4fac3c7e9b9
Хахель Владислав <vlad_hahel@mail.ru>
2018-09-11 19:42:54 +0300

    Average marks project

---
+++ git/AvgMarks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvgMarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(1,"Valera","2-1");
+            Student student2 = new Student(2,"Katya","2-2");
+            Student student3 = new Student(3, "Zhenya", "2-1");
+
+            student1.Score = new int[] {9,10,10,10,10};
+            student2.Score = new int[] {4,5,6,4,4,4,5,6,8,5,6};
+            student3.Score = new int[] { 2,10,5};
+
+            double []mas = new double[] { student1.GetAvgScore(student1.Score), student2.GetAvgScore(student2.Score), student3.GetAvgScore(student3.Score) };
+            student1.GetInfo();
+            student2.GetInfo();
+            student3.GetInfo();
+            Console.WriteLine("Students Count: {1},Group AvgScore: {0}",Student.GetGroupAvgScore(mas), Student.studentsCount);
+
+            
+         }
+    }
+}
---
+++ git/AvgMarks/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvgMarks
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public int[] Score { get; set; }
+        public double AvgScore { get; set; }
+        public static int studentsCount=0;
+
+        public Student (int id,String name,string group)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Group = group;
+            studentsCount++;
+        }
+
+       public  double GetAvgScore (int[] masScore)
+            {
+            double avgScore=0;
+            for (int i = 0; i < masScore.Length; i++)
+            {
+                avgScore += (double)masScore[i];
+            }
+            avgScore = avgScore / masScore.Length;
+            return avgScore;
+            }
+        public static double GetGroupAvgScore( double []masScore)
+        {
+            double avgScore = 0;
+            for (int i = 0; i < masScore.Length; i++)
+            {
+                avgScore += (double)masScore[i];
+            }
+            avgScore = avgScore / masScore.Length;
+            return avgScore;
+        }
+       public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name} \tGroup:{Group} \tAverage Score: {GetAvgScore(Score)}\n");
+        }
+
+       
+    }
+}
