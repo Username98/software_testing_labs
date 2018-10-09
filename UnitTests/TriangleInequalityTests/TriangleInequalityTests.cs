c65dd1a02422bc3f7db13d100496967f5e0a9767
Хахель Владислав <vlad_hahel@mail.ru>
2018-10-09 20:15:24 +0300

    redudant code has deen removed

--- UnitTests/TriangleInequalityTests/TriangleInequalityTests.cs
+++ UnitTests/TriangleInequalityTests/TriangleInequalityTests.cs
@@ -6,112 +6,56 @@ namespace TriangleInequalityTests
     [TestClass]
     public class TriangleInequalityTests
     {
+        TriangleInequality.Triangle t = new TriangleInequality.Triangle();
         [TestMethod]
         public void AllSidesAreEqual_TrueReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-
             Assert.AreEqual(true, t.TriangleInequality(5, 5, 5));
         }
         [TestMethod]
         public void MinSidesDifference_TrueReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = 5;
-            double b = 5;
-            double c = 9.99999999;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = true;
-            Assert.AreEqual(expected,actual);
+            Assert.AreEqual(true, t.TriangleInequality(5, 5, 9.999999));
         }
         [TestMethod]
         public void MinSidesDifference_FalseReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = 4.99999999;
-            double b = 5;
-            double c = 10;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+          Assert.AreEqual(false, t.TriangleInequality(4.99999, 5, 10));
         }
         [TestMethod]
         public void ZeroLengthSides_FalseReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = 4.99999999;
-            double b = 5;
-            double c = 0;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(false, t.TriangleInequality(4.99999, 5, 0));
         }
         [TestMethod]
         public void NegativeLengthSides_FalseReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = 4.99999999;
-            double b = -5;
-            double c = 10;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(false, t.TriangleInequality(4.99999, -5, 10));
         }
         [TestMethod]
         public void DecimalLengthSides_TrueReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = 4.99999999;
-            double b = 0b1010;
-            double c = 10;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = true;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(true, t.TriangleInequality(4.99999, 0b1010,10));
         }
         [TestMethod]
         public void HexLengthSides_TrueReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = 4.99999999;
-            double b = 0b1010;
-            double c = 0xd;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = true;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(true, t.TriangleInequality(4.99999, 0b1010, 0xd));
         }
         [TestMethod]
         public void MoreThanMaxDoubleLengthSides_FalseReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = double.MaxValue+100;
-            double b = double.MaxValue;
-            double c = double.MaxValue;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(false, t.TriangleInequality(double.MaxValue+100, double.MaxValue, double.MaxValue));
         }
         [TestMethod]
         public void LessThanMinLengthSides_falseReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = double.MinValue;
-            double b = double.MinValue;
-            double c = double.MinValue;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(false, t.TriangleInequality(double.MinValue, double.MinValue, double.MinValue));
         }
         [TestMethod]
         public void NaNSides_FalseReturned()
         {
-            TriangleInequality.Triangle t = new TriangleInequality.Triangle();
-            double a = 4.99999999;
-            double b = double.NaN;
-            double c = double.NaN;
-            bool actual = t.TriangleInequality(a, b, c);
-            bool expected = false;
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(false, t.TriangleInequality(4.99999,double.NaN,double.NaN));
         }
-
     }
 }
