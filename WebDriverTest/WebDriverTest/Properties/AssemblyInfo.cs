1b9eb911ee43bd92a2427560263dc59439d21432
Хахель Владислав <vlad_hahel@mail.ru>
2018-10-21 22:50:58 +0300

    Selenium Web Driver added

---
+++ WebDriverTest/WebDriverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebDriverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebDriverTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc2187fc-b374-4c08-83fd-1a4ba3c0f96a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebDriverTest/WebDriverTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace WebDriverTest
+{
+    
+
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver chrome = new ChromeDriver(@"C:\Univer\4k\software_testing_labs\WebDriverTest\WebDriverTest\bin\Debug");
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            chrome.Navigate().GoToUrl("https://ostrovok.ru/?tabs=aero&sid=14b52feb-4ba7-4912-866d-ef2ba31b8b16");
+           
+            chrome.FindElement(By.XPath(@"//*[@id=':0']/div/div/div[1]/div/div[3]/div/div/div/div[1]/div/div[1]/div[1]/div/div/div[2]/input")).SendKeys("Москва, Россия");
+            
+            chrome.FindElement(By.XPath(@"//*[@id=':0']/div/div/div[1]/div/div[3]/div/div/div/div[1]/div/div[1]/div[3]/div/div/div[2]/input")).SendKeys("Москва, Россия");
+
+            chrome.FindElement(By.XPath(@"//*[@id=':0']/div/div/div[1]/div/div[3]/div/div/div/div[2]/div[2]/div[1]/button")).Click();
+
+           
+            bool actual = false;
+            Thread.Sleep(500);
+            try
+            {
+               // WebDriverWait _wait = new WebDriverWait(chrome, new TimeSpan(0, 0, 20));
+                //_wait.Timeout = 500;
+                chrome.FindElement(By.XPath("/html/body/div[1]/div/div[3]/div[1]/div[1]/div[2]/div/div[10]/div[2]/div[5]/div[2]/div[9]/div[1]/div[3]/div[2]"));
+                //    chrome.FindElement(By.ClassName("ticket-scroll-container"));
+               // _wait.Until(d => d.FindElement(By.ClassName("ticket-scroll-container")));
+            }
+            catch (Exception)
+            {
+                actual = true;               
+            }
+     
+             Assert.AreEqual(true,actual);
+          
+        }
+       [TestCleanup]
+        public void TearDown()
+        {
+            chrome.Quit();
+        }
+    }
+}
