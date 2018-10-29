ed962cf369560e04a0f3df3a987cae8c6ef57ff5
Хахель Владислав <vlad_hahel@mail.ru>
2018-10-29 16:01:54 +0300

    PageObject added

---
+++ PageObject/PageObject/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace WebDriverTest
+{
+    [TestClass]
+    public class OstrovokTests
+    {
+        public IWebDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            this.Driver = new ChromeDriver(@"C:\Univer\4k\software_testing_labs\WebDriverTest\WebDriverTest\bin\Debug");
+            this.Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(30));
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void SearchTicketWithoutAgreement()
+        {
+            OstrovokSearchFrom ticketForm = new OstrovokSearchFrom(this.Driver);
+            ticketForm.ValidateResults(ticketForm.SearchAndClickElement());
+        }
+    }
+}
---
+++ PageObject/PageObject/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace WebDriverTest
+{
+    [TestClass]
+    public class OstrovokSearchFrom
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = @"https://ostrovok.ru/?tabs=aero&sid=14b52feb-4ba7-4912-866d-ef2ba31b8b16";
+        // IWebDriver chrome = new ChromeDriver(@"C:\Univer\4k\software_testing_labs\WebDriverTest\WebDriverTest\bin\Debug");
+        public OstrovokSearchFrom(IWebDriver browser)
+        {
+            this.driver = browser;
+            this.driver.Manage().Window.Maximize();
+            PageFactory.InitElements(browser, this);
+        }
+        [TestMethod]
+        public bool SearchAndClickElement()
+        {
+            bool actual = false;
+            try
+            {
+                driver.Navigate().GoToUrl(url);
+
+                driver.FindElement(By.XPath(@"//*[@id=':0']/div/div/div[1]/div/div[3]/div/div/div/div[1]/div/div[1]/div[1]/div/div/div[2]/input")).SendKeys("Москва, Россия");
+
+                driver.FindElement(By.XPath(@"//*[@id=':0']/div/div/div[1]/div/div[3]/div/div/div/div[1]/div/div[1]/div[3]/div/div/div[2]/input")).SendKeys("Санкт-Петербург, Россия");
+
+                driver.FindElement(By.XPath(@"//*[@id=':0']/div/div/div[1]/div/div[3]/div/div/div/div[4]/div/label/div[1]")).Click();
+
+                driver.FindElement(By.XPath(@"//*[@id=':0']/div/div/div[1]/div/div[3]/div/div/div/div[2]/div[2]/div[1]/button")).Click();
+
+            }
+            catch (Exception)
+            {
+               actual = true;
+            }
+            return actual;
+                   }
+        public void ValidateResults(bool actual)
+        {
+           Assert.IsTrue(actual);
+        }
+    }
+}
